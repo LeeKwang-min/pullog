abcae9ff3363b891ffd81be4f2384f29237f5ff5
LeeKwang-min <dltkdtn56@naver.com>
2024-08-21 21:50:00 +0900

    [수정] 로그인 페이지 shadcn ui 템플릿 변경

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,5 +1,9 @@
 import { cn } from "@/lib/utils";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <main className={cn("w-full h-full")}>{children}</main>;
+  return (
+    <main className={cn("w-full h-dvh flex items-center justify-center")}>
+      {children}
+    </main>
+  );
 }
