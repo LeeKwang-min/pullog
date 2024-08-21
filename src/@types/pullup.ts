b40264d089ec40e2cd997890cb67b868d93ed9d7
LeeKwang-min <dltkdtn56@naver.com>
2024-08-21 22:01:07 +0900

    [기능] 화면 구성을 위한 임시 데이터 셋 구성 - 타입 지정, 임시 데이터 생성

---
+++ src/@types/pullup.ts
@@ -0,0 +1,36 @@
+export type TDayConst =
+  | "MONDAY"
+  | "TUESDAY"
+  | "WEDNESDAY"
+  | "THURSDAY"
+  | "FRIDAY"
+  | "SATURDAY"
+  | "SUNDAY";
+
+export type TPullupSetTime = {
+  minute: number;
+  second: number;
+};
+
+export type TPullupSnsLink = {
+  instar?: string;
+  youtube?: string;
+};
+
+export interface ISPullupSetData {
+  id: number;
+  count: number;
+  time: TPullupSetTime;
+}
+
+export interface IPullupData {
+  date: string;
+  day: TDayConst;
+  setData: ISPullupSetData[];
+  snsLink?: TPullupSnsLink;
+}
+
+export interface IGetPullupResponse {
+  status: number;
+  data: IPullupData[];
+}
